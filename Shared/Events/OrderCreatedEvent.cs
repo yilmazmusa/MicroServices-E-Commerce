f92f80a200142e664730026efdfc419c880cc560
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-08-28 23:46:31 +0300

    Order ve Stok Servisleri için OrderCreatedEvet oluşturuldu.

---
+++ Shared/Events/OrderCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Events.Common;
+using Shared.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Events
+{
+    public class OrderCreatedEvent : IEvent // Order(Sipariş) oluşurken gerekli olan propertyler. Biz Eventimizi oluşturduk sipariş oluştuğunda biz bu Eventi fırlatıcaz.
+        //Yani diyoruz ki şu BuyerId li kullanıcının yapmış olduğu şu  OrderId li siparişin içerisinde  OrderItemMessage ın içindeki şu Üründen(ProductId) şu kadar tane(Count)  ürün siparişe eklendi bunları stoktan düş diyeceğiz Event ile.
+    {
+        public Guid OrderId { get; set; }
+        public Guid BuyerId { get; set; }
+        public List<OrderItemMessage> OrderItems { get; set; }
+    }
+}
---
+++ Shared/Messages/OrderItemMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Messages
+{
+    public class OrderItemMessage //Order oluştururken asenkron bir şekilde Stok servisine işi teslim ederken bize ürünün stoğunu güncelleyebilmemiz için Order(Sipariş) te hangi Üründen(ProductId) şu kadar tane(Count) alındı bunları stoktan düş diyeceğiz.
+    {
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
