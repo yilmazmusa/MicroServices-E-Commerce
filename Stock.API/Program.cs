3775b465b3d2181c725c42036963179d319920d6
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-08-29 00:51:55 +0300

    Stock.API servisi eklendi.

---
+++ Stock.API/Program.cs
@@ -0,0 +1,38 @@
+using MassTransit;
+using Stock.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((context, _configurator) =>
+    {
+        _configurator.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
+
+builder.Services.AddSingleton<MongoDBService>(); // MongoDBService' nin IOC ile irtibatlý olmasý için bunu gelip bu þekilde IOC Container a ekliyoruz.
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Stock.API/Services/MongoDBService.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace Stock.API.Services
+{
+    public class MongoDBService // Bu MongoDBService' in IOC ile irtibatlı olması için bunu gidip Program.cs de builder.Services.AddSingleton<MongoDBService>(); şeklinde IOC Container a    ekliyoruz.
+    {
+        readonly IMongoDatabase _mongoDatabase; // IMongoDatabase interfacesinden bir referans aldık.
+
+        public MongoDBService(IConfiguration configuration)
+        {
+                MongoClient   client = new(configuration.GetConnectionString("MongoDB"));
+                _mongoDatabase = client.GetDatabase("StockAPIDB");
+        }
+    }
+}
