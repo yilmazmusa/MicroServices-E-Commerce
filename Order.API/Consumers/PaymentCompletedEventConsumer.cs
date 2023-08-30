bcd712e8607d726e859f1d64a023cdd5dea1f562
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-08-30 11:53:23 +0300

    PaymentCompletedEvent eklendi.

--- Order.API/Consumers/PaymentCompletedEventConsumer.cs
+++ Order.API/Consumers/PaymentCompletedEventConsumer.cs
@@ -1,6 +1,25 @@
-﻿namespace Order.API.Consumers
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+using Shared.Events;
+
+namespace Order.API.Consumers
 {
-    public class PaymentCompletedEventConsumer
+    public class PaymentCompletedEventConsumer : IConsumer<PaymentCompletedEvent>
     {
+        readonly OrderAPIDBContext _orderAPIDBContext;
+
+        public PaymentCompletedEventConsumer(OrderAPIDBContext orderAPIDBContext)
+        {
+            _orderAPIDBContext = orderAPIDBContext;
+        }
+
+        public async Task Consume(ConsumeContext<PaymentCompletedEvent> context)
+        {
+          Order.API.Models.Entities.Order? order =   await _orderAPIDBContext.Orders.FirstOrDefaultAsync(o => o.OrderId == context.Message.OrderId);
+
+            order.OrderStatu = Models.Enums.OrderStatus.Completed;//İlgili Order ın statusunu Completed'a çektk.
+            await _orderAPIDBContext.SaveChangesAsync();
+        }
     }
 }
--- Payment.API/Consumers/StockReservedEventConsumer.cs
+++ Payment.API/Consumers/StockReservedEventConsumer.cs
@@ -1,12 +1,38 @@
 ﻿using MassTransit;
+using Shared.Events;
 
 namespace Payment.API.Consumers
 {
-    public class StockReservedEventConsumer : IConsumer<StockReservedEventConsumer>
+    public class StockReservedEventConsumer : IConsumer<StockReservedEvent>
     {
-        public  Task Consume(ConsumeContext<StockReservedEventConsumer> context)
+        readonly IPublishEndpoint _publishEndpoint;
+
+        public StockReservedEventConsumer(IPublishEndpoint publishEndpoint)
         {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public  Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            //StockReserved işlemlerinde bir sorun yok ya yani o ürünün stoğu istenenden fazlaysa sorn yok demektir.Artık ödeme işlemlerine geçilebilir.
+            //Ödeme işlemleri
+
+            if (true)
+            {
+                PaymentCompletedEvent paymentCompletedEvent = new()
+                {
+                    OrderId = context.Message.OrderId,
+                    //Burda ödeme için TotalPrice ve BuyerId gerekiyor aslında mış gibi yaptık burda.
+                };
+                _publishEndpoint.Publish(paymentCompletedEvent); // Publish ettik artık.
+            }
+            else
+            {
+                //Ödeme sırasında hata
+            }
             return Task.CompletedTask;
         }
+
+       
     }
 }
