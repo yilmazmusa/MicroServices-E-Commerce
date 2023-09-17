d4933c9b758c44c9812086d6e3e90b1bbd41df28
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-09-17 15:39:22 +0300

    Event Driven Fix.

--- Stock.API/Services/MongoDBService.cs
+++ Stock.API/Services/MongoDBService.cs
@@ -1,6 +1,6 @@
 ﻿using MongoDB.Driver;
 
-namespace Stock.API.Services
+namespace Stock.API.Services  
 {
     public class MongoDBService // Bu MongoDBService' in IOC ile irtibatlı olması için bunu gidip Program.cs de builder.Services.AddSingleton<MongoDBService>(); şeklinde IOC Container a    ekliyoruz.
     {
@@ -8,13 +8,13 @@ namespace Stock.API.Services
 
         public MongoDBService(IConfiguration configuration)
         {
-                MongoClient   client = new(configuration.GetConnectionString("MongoDB")); //ConnectionStringi aldık.
-                _mongoDatabase = client.GetDatabase("StockDB");
+                MongoClient   client = new(configuration.GetConnectionString("MongoDB"));//MongoClient sınıfı bizim MongoDB nin sunucusuna bağlanmamızı sağlıyo zaten MongoDB  sunucusunun ConnectionStringini alıyor. client sunucuya bağlanıyor ve bize veritabanını getiriyor.
+            _mongoDatabase = client.GetDatabase("StockDB"); //StockDB adındaki Veritabanımızı _mongoDatabase referansına verdik.
         }
 
 
 
-        //MongoDB de bir DATABASE(StockAPIDB) den  veri okumak için  IMongoCollection türünden bir func oluşturduk.
+        //MongoDB de bir DATABASE(StockAPIDB) den  veri okumak için  IMongoCollection türünden bir func oluşturduk.GetCollection fonksiyonu bize  StockAPIDB(_mongoDatabase) den GetCollection fonksiyonu ile T ye karşılık gelen collectionu getirecek.Yani hangi Entity veriliyorsa o entity e karşılık gelen collection getirilecek.
         public IMongoCollection<T> GetCollection<T>() => _mongoDatabase.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
     }
 }
